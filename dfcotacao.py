3ddcc5d52aed0c68c6bae1de09529be2028e98af
ViniciusSimulacro <delucca86@gmail.com>
2023-03-16 20:48:09 -0300

    commit test

--- dfcotacao.py
+++ dfcotacao.py
@@ -6,4 +6,10 @@ import pandas as pd
 cotacoes = requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/30')
 cotacoes = cotacoes.json()
 df = pd.DataFrame.from_dict(cotacoes)
+
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+df['dia'] = df['timestamp'].dt.day
+df['mes'] = df['timestamp'].dt.month
+df['ano'] = df['timestamp'].dt.year
+
 print(df.fillna(method="ffill"))
