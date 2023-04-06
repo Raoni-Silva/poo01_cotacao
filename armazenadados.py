ac45265804519bbd2272c06f31b572efae0a7df3
ViniciusSimulacro <delucca86@gmail.com>
2023-04-06 06:59:13 -0300

    modularizacao de funcoes

--- armazenadados.py
+++ armazenadados.py
@@ -7,13 +7,20 @@ from time import time
 cotacoes = requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/1')
 cotacoes = cotacoes.json()
 
-print(cotacoes[0]['name'])
-print(cotacoes[0]['bid'])
+nome = cotacoes[0]['name']
+cotacao = cotacoes[0]['bid']
 
 data = cotacoes[0]['timestamp']
 data = time()
 data = (datetime.utcfromtimestamp(data).strftime('%Y-%m-%d'))
 data = datetime.strptime(data, '%Y-%m-%d')
 data_str = data.strftime('%Y-%m-%d')
+data = data_str
 
-print(data_str)
+lista = []
+lista.append({'nome': nome, 'cotacao': cotacao, 'data': data}) 
+
+df = pd.DataFrame.from_dict(lista)
+print(df)
+
+
--- dfcotacao.py
+++ dfcotacao.py
@@ -1,15 +1,27 @@
 import requests
 import json
 import pandas as pd
+from datetime import datetime as dt
 
+class Cotacao():
+    def __init__(self):
+        self.modulo = 'cotacao'
+        self.api = 'https://economia.awesomeapi.com.br/json/daily/:moeda?'
 
-cotacoes = requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/30')
-cotacoes = cotacoes.json()
-df = pd.DataFrame.from_dict(cotacoes)
+    def printCotacao(self):
+        funcao = 'Print_Cotacao'
+        cotacoes = requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/30')
+        cotacoes = cotacoes.json()
+        df = pd.DataFrame.from_dict(cotacoes)
 
-df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-df['dia'] = df['timestamp'].dt.day
-df['mes'] = df['timestamp'].dt.month
-df['ano'] = df['timestamp'].dt.year
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+        df['dia'] = df['timestamp'].dt.day
+        df['mes'] = df['timestamp'].dt.month
+        df['ano'] = df['timestamp'].dt.year
+        print(f'Rodando função: {funcao}')
+        print(df.fillna(method="ffill"))
 
-print(df.fillna(method="ffill"))
+    def cotacaoDoDia(self,d0 = dt.strftime(dt.today(),r'%Y%m%d')):
+        x = self.api + 'start_date='+ d0 + '&end_date='+ d0
+        print(x)
+
