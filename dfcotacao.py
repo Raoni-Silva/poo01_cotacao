21111e9a839388b1b47174e2bc38c77e583a9599
Raoni Silva <raoni.henriques@gmail.com>
2023-04-26 20:05:32 -0300

    Inicio da funcao de lista de moedas

--- dfcotacao.py
+++ dfcotacao.py
@@ -7,30 +7,54 @@ import auxiliar as aux
 class Cotacao():
     def __init__(self):
         self.modulo = 'cotacao'
-        self.api = 'https://economia.awesomeapi.com.br/json/daily/USD-BRL/?'
+        self.api_cotacao = 'https://economia.awesomeapi.com.br/json/daily/'
+        self.api_codigos = 'https://economia.awesomeapi.com.br/xml/available'
+    # def s():       
+    #     df = pd.DataFrame.from_dict(cotacoes)
 
-    def s():       
-        df = pd.DataFrame.from_dict(cotacoes)
+    #     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+    #     df['dia'] = df['timestamp'].dt.day
+    #     df['mes'] = df['timestamp'].dt.month
+    #     df['ano'] = df['timestamp'].dt.year
+    #     print(f'Rodando função: {funcao}')
+    #     print(df.fillna(method="ffill"))
 
-        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-        df['dia'] = df['timestamp'].dt.day
-        df['mes'] = df['timestamp'].dt.month
-        df['ano'] = df['timestamp'].dt.year
-        print(f'Rodando função: {funcao}')
-        print(df.fillna(method="ffill"))
+    def cotacaoDoDia(self, d0 = dt.strftime(dt.today(),r'%Y%m%d'), moeda='USD-BRL'):
+        url = self.api + moeda + '/?start_date='+ d0 + '&end_date=' + d0
+        x = aux.pooAux()
+        df = x.getApi(url)     
+        print(df)
+        
+    def codigoMoedas(self):
+        response = requests.get(self.api_codigos)
 
-    def cotacaoDoDia(self,d0 = dt.strftime(dt.today(),r'%Y%m%d'), d1=None):
-        if d1 == None:
-            url = self.api + 'start_date='+ d0 + '&end_date='+ d0
-            print(url)
-            x = aux.pooAux()
-            df = x.getApi(url)     
-            print(df.fillna(method="ffill"))
+        if response.status_code == 200:
+            text = response.content
+            print(pd.read_xml(text))
+            
+            # lines = text.splitlines()
+            # for line in lines:
+            #     print(line)
         else:
-            # d1 = dt.strftime(d1,r'%Y%m%d')
-            url = self.api + 'start_date='+ d0 + '&end_date='+ d1
-            print(url)
-            x = aux.pooAux()
-            df = x.getApi(url)     
-            print(df.fillna(method="ffill"))
+            print("Não foi possível obter o conteúdo")
+        
+        
+       
+
+
+    #def cotacaoDoDia(self,d0 = dt.strftime(dt.today(),r'%Y%m%d'), d1=None, moeda='USD-BRL'):
+        # if d1 == None:
+        #     d1 = d0
+        #     url = self.api + moeda + '/?start_date='+ d0 + '&end_date='+ d1
+        #     print(url)
+        #     x = aux.pooAux()
+        #     df = x.getApi(url)     
+        #     print(df.fillna(method="ffill"))
+        # else:
+        #     # d1 = dt.strftime(d1,r'%Y%m%d')
+        #     url = self.api + 'start_date='+ d0 + '&end_date='+ d1
+        #     print(url)
+        #     x = aux.pooAux()
+        #     df = x.getApi(url)     
+        #     print(df.fillna(method="ffill"))
             
--- pooplan.py
+++ pooplan.py
@@ -9,13 +9,16 @@ import auxiliar
 if sys.argv[1] == 'cotacao': 
     dc.Cotacao().printCotacao()
 
-if sys.argv[1] == 'dia':
+if sys.argv[1] == 'cotacaoDia':
     if len(sys.argv) == 2:
         dc.Cotacao().cotacaoDoDia()
-    if len(sys.argv) == 3:
-        d0 = sys.argv[2]
-        dc.Cotacao().cotacaoDoDia(d0)
-    if len(sys.argv) == 4:
-        d0 = sys.argv[2]
-        d1 = sys.argv[3]
-        dc.Cotacao().cotacaoDoDia(d0,d1)
+    # if len(sys.argv) == 3:
+    #     d0 = sys.argv[2]
+    #     dc.Cotacao().cotacaoDoDia(d0)
+    # if len(sys.argv) == 4:
+    #     d0 = sys.argv[2]
+    #     d1 = sys.argv[3]
+    #     dc.Cotacao().cotacaoDoDia(d0,d1)
+
+if sys.argv[1] == 'codigosMoeda':
+    dc.Cotacao().codigoMoedas()
